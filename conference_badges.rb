2f47a0b54a6f9f8a854b856a0d45d95f5145366c
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-12 04:51:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,7 +29,7 @@ end
 # Hint: Remember that methods can call other methods. If the return value of assign_rooms is an array of room assignments, how can you print out each assignment? You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
 
 def printer(array)
-  batch_badge_creator.each do |message|
+  array.each do |message|
     puts "#{message}"
   end 
 end 
