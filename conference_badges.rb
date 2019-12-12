e17a73a90e79bedcdcf48bfdc5b00cf9cf0480e4
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-12 04:37:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+# Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
+
+def batch_badge_creator(array)
+  batch_messages = []
+  array.each do |person|
+    batch_messages <<  "Hello, my name is #{person}."
+  end 
+  return batch_messages
+end 
+
+# return a list of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
+
+def assign_rooms 
+  
+end 
